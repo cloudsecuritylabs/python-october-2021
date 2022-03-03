a2c7cf86dc59d03d62b9687b5784fc71fba096f4
Ankan Basu <basu.ankan@gmail.com>
2022-03-02 22:49:58 -0500

    updated files for class1

--- class_1_intro/0.0.py
+++ class_1_intro/0.0.py
@@ -1,42 +1,35 @@
-# import this
-import sys
-# print os information
-print(sys.platform)
+import this
+# import sys
+# # print os information
+# print(sys.platform)
 
 # print python version
-print(sys.version)
-
-key = 'my_var'
-value = 1.234
-
-formatted = f'{key} = {value}'
-print(formatted)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# print(sys.version)
+# print(sys.version.isalnum())
 #
-# # bytes data type
-# a = b'h\x65llo'
-# print(list(a))
-# print(a)
+# key = 'my_var'
+# value = 1.234
 #
-# # str datatype
-# a = 'a\u0300 propos'
-# print(list(a))
-# print(a)
+# formatted = f'{key} = {value}'
+# print(formatted)
+
+# test some basic user input
+# x = input("give me a number")
+# print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ class_1_intro/in_class_datatype.py
@@ -0,0 +1,30 @@
+# find out the age
+birth_year = input("What is your birth year? ")
+print(birth_year)
+#type casting
+string age = 2022 - (birth_year)
+print(age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# age = 2021 - int(year)
+# print(age)
+
+# color = input("What is your fav color? ")
+# print(color)
